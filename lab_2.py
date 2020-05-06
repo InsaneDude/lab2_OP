44a48b9538a3561eb8a5c609284a6a1d702ba7d7
insane_dude <insane.dude.btw@gmail.com>
2020-05-06 16:31:29 +0300

    added converting int to str

--- lab_2.py
+++ lab_2.py
@@ -8,3 +8,14 @@ with open("testing_new.csv", "r") as csvfile:
         inforeaded[0].remove('')
     for i in range(len(inforeaded)):
         print(inforeaded[i])
+
+
+moving_to_next = 0
+for k in range(len(inforeaded)):
+    while str in inforeaded[k]:
+        inforeaded[k][moving_to_next] = int(inforeaded[k][moving_to_next])
+        # if TypeError:
+        #    continue
+        moving_to_next += 1
+    moving_to_next = 0
+print(inforeaded)
