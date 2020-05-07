229472ed0ad86bc393abe836e50c79d4f649f1f2
insane_dude <insane.dude.btw@gmail.com>
2020-05-08 00:04:59 +0300

    made reading all file from needed directory

--- lab_2.py
+++ lab_2.py
@@ -1,26 +1,29 @@
-with open("testing_new.csv", "r") as csvfile:
-    inforeaded = []
-    for readed_now in csvfile:
-        inforeaded.append(readed_now.split(";"))
-    for k in range(len(inforeaded)):
-        inforeaded[k] = list(map(lambda s: s.strip(), inforeaded[k]))
-    while '' in inforeaded[0]:
-        inforeaded[0].remove('')
+import os
 
 
-number_of_countries = inforeaded.pop(0)
-# for i in range(len(inforeaded)):
-#     print(inforeaded[i])
+way_to_directory = str(input("Введіть шлях до папки : "))
+files_in_dir_list = os.listdir(way_to_directory)
+inforeaded = []
+os.chdir(way_to_directory)
+for i in range(len(files_in_dir_list)):
+    with open(files_in_dir_list[i], "r") as csvfile:
+        for readed_now in csvfile:
+            inforeaded.append(readed_now.split(";"))
+        for k in range(len(inforeaded)):
+            inforeaded[k] = list(map(lambda s: s.strip(), inforeaded[k]))
+inforeaded.sort()
+for checking_array in range(int(len(inforeaded)/2)):
+    inforeaded.pop(0)
+
+
+number_of_countries = len(inforeaded)
 countries = []
 for k in range(len(inforeaded)):
     countries.append(inforeaded[k][0])
-print(countries)
-points_of_every_country = [[countries[k], 0] for k in range(int(number_of_countries[0]))]
-# print(points_of_every_country)
-
+points_of_every_country = [[countries[k], 0] for k in range(number_of_countries)]
 for counting in range(1, len(inforeaded)+1):
     temp_collecting_columns = []
-    for counting_next in range(int(number_of_countries[0])):
+    for counting_next in range(number_of_countries):
         temp_collecting_columns.append(int(inforeaded[counting_next][counting]))
         points_of_every_country[counting_next - 1][1] += 1
     winner_1 = max(temp_collecting_columns)
